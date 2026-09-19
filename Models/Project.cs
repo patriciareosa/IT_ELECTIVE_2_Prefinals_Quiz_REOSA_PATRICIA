647909028794966bef70bf9ab844dd6f60073be8
patriciareosa <patriciareosa@gmail.com>
2026-09-19 17:39:06 +0800

    Add files via upload

---
+++ Models/Project.cs
@@ -0,0 +1,17 @@
+﻿namespace MyPortfolio.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = "";
+
+        public string Description { get; set; } = "";
+
+        public string Technologies { get; set; } = "";
+
+        public string GithubUrl { get; set; } = "";
+
+        public string ImageUrl { get; set; } = "";
+    }
+}
